00c32d9c876902a0b5df55978da0f9c36f0afe3b
Gregory <grihabor@mail.ru>
2016-10-02 02:19:52 +0300

    works but smth wrong...

--- mfk.py
+++ mfk.py
@@ -2,7 +2,8 @@ from urllib import request
 from bs4 import BeautifulSoup
 
 BASE_URL = 'http://new.mfk.msu.ru/course/'
-
+student_keys = ['name', 'faculty', 'study_mode', 'degree', 'year']
+course_keys = ['name', 'faculty', 'online', 'status']
 
 def courses_list_url(page = 1):
     return '{}list?page={}'.format(BASE_URL, page + 1)
@@ -29,9 +30,8 @@ def parse_list_page(html):
         if line.string is not None:
             continue
 
-        course = {name : item.string for name, item in
-            zip(['name', 'faculty', 'online', 'status'], line.contents)}
-        course['id'] = line['id']
+        course = {name : item.string for name, item in zip(course_keys, line.contents)}
+        course['id'] = int(line['id'])
         courses.append(course)
 
     return courses
@@ -47,10 +47,16 @@ def get_courses():
         with request.urlopen(courses_list_url(page)) as response:
             html = response.read()
             courses += parse_list_page(html)
-    return courses
+    return {course['id']: course for course in courses}
 
-# Changes course in place!
-def parse_course_details_and_people(course):
+def parse_course_details_and_students(course):
+    """
+    Search for course page using course['id'], add extra information
+    and list of enrolled students to course dictionary in place
+
+    :param course: dictionary
+    :return: students -- dictionary
+    """
     with request.urlopen(course_url(course['id'])) as response:
         page = BeautifulSoup(response.read(), 'lxml')
         detailed_info = page.find('div', {'class': 'well'})
@@ -62,15 +68,49 @@ def parse_course_details_and_people(course):
             I just need number total and left places
         '''
 
-        left_places, total_places = [x.strip() for x in
+        taken_places, total_places = [x.strip() for x in
                                      detailed_info[-1].contents[-1].split('/')]
-        print(left_places, total_places)
-        course['left_places'] = left_places
+        course['taken_places'] = taken_places
         course['total_places'] = total_places
 
+        # parse students
+        students = {}
+        table = page.find('table', {'class': 'table'})
+        for row in table.find('tbody').contents:
+            # skip empty rows
+            if row.string is not None:
+                continue
 
+            student = {key : value.text for key, value in zip(student_keys, row.contents)}
+            student['name'] = student['name'].split()
+            student['data-key'] = int(row['data-key'])
+            students[student['data-key']] = student
+
+        course['students'] = {int(student_id) for student_id in students.keys()}
+
+    return students
 
 courses = get_courses()
-for course in courses:
-    parse_course_details_and_people(course)
-print(courses)
+students = {}
+for course_id, course in courses.items():
+    print('Parsing course {}: {}'.format(course_id, course['name']))
+    course_students = parse_course_details_and_students(course)
+
+    # insert course_students into students
+    for id, student in course_students.items():
+        if id in students:
+            students[id]['courses'].insert(course_id)
+        else:
+            student['courses'] = {course_id}
+            students[id] = student
+
+print('Done')
+
+course_number = {}
+for id, student in students.items():
+    try:
+        course_number[len(student['courses'])] += 1
+    except KeyError:
+        course_number[len(student['courses'])] = 0
+
+print(course_number)
