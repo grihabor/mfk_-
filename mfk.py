eaf418de7bfcf23a726f876f85001db5e80cd622
Gregory <grihabor@mail.ru>
2017-01-26 22:31:01 +0300

    moved html parsing to MFKParser class

--- mfk.py
+++ mfk.py
@@ -32,21 +32,10 @@ course_columns['online'] = 'Online'
 course_columns['status'] = 'Статус'
 
 
-class MFKManager:
+class MFKParser:
 
-    def __init__(self, name_ru='', msu_faculty_id='', is_online='',
-                 course_status_id='', fall_year='', semester_number=''):
-        self.url_params = '&'.join(URL_PARAMS_TEMPLATE).format(
-            name_ru=name_ru, msu_faculty_id=msu_faculty_id,
-            is_online=is_online, course_status_id=course_status_id,
-            fall_year=fall_year, semester_number=semester_number
-        )
-
-    def courses_list_url(self, page=1):
-        return '{}list?{}'.format(BASE_URL, self.url_params + '&page={}'.format(page + 1))
-
-    def course_url(self, course_id):
-        return '{}view?id={}'.format(BASE_URL, course_id)
+    def __init__(self, **kwargs):
+        self.url_params = '&'.join(URL_PARAMS_TEMPLATE).format(**kwargs)
 
     def get_num_pages(self, html):
         page = BeautifulSoup(html, 'lxml')
@@ -69,17 +58,6 @@ class MFKManager:
             courses.append(course)
 
         return courses
-
-    def get_courses(self):
-        courses = []
-
-        html = request.urlopen(BASE_URL + 'list').read()
-        num_pages = self.get_num_pages(html)
-        for page in range(num_pages):
-            with request.urlopen(self.courses_list_url(page)) as response:
-                html = response.read()
-                courses += self.parse_list_page(html)
-        return {course['id']: course for course in courses}
 
     def parse_course_details_and_students(self, course):
         """
@@ -119,6 +97,36 @@ class MFKManager:
                 students.append(student)
 
         return students
+
+    def courses_list_url(self, page=1):
+        return '{}list?{}'.format(BASE_URL, self.url_params + '&page={}'.format(page + 1))
+
+    def course_url(self, course_id):
+        return '{}view?id={}'.format(BASE_URL, course_id)
+
+
+class MFKManager:
+
+    def __init__(self, name_ru='', msu_faculty_id='', is_online='',
+                 course_status_id='', fall_year='', semester_number=''):
+
+        self.parser = MFKParser(**dict(
+            name_ru=name_ru, msu_faculty_id=msu_faculty_id,
+            is_online=is_online, course_status_id=course_status_id,
+            fall_year=fall_year, semester_number=semester_number
+        ))
+
+
+    def get_courses(self):
+        courses = []
+
+        html = request.urlopen(BASE_URL + 'list').read()
+        num_pages = self.parser.get_num_pages(html)
+        for page in range(num_pages):
+            with request.urlopen(self.parser.courses_list_url(page)) as response:
+                html = response.read()
+                courses += self.parser.parse_list_page(html)
+        return {course['id']: course for course in courses}
 
     def students_course_count(self, students):
         course_count = {}
@@ -190,7 +198,7 @@ class MFKManager:
                 break
             '''
             print('Parsing course {}: {}'.format(course_id, course['name']))
-            students_dup.append(self.parse_course_details_and_students(course))
+            students_dup.append(self.parser.parse_course_details_and_students(course))
 
         # Merge students_dup in one list
         students_dup = [student for course_students in students_dup for student in course_students]
